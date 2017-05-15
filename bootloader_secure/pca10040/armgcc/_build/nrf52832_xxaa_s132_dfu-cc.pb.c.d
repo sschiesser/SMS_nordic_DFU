093763ecaf8fa5d257fd213493a79ba9f8e03ad6
Sebastien Schiesser <sebastien.schiesser@zhdk.ch>
2017-05-15 09:56:13 +0200

    Successful OTA Update... issue with disconnection from App Advertiser and reconnection to DFU Advertiser

---
+++ bootloader_secure/pca10040/armgcc/_build/nrf52832_xxaa_s132_dfu-cc.pb.c.d
@@ -0,0 +1,69 @@
+_build/nrf52832_xxaa_s132_dfu-cc.pb.c.o: ../../dfu-cc.pb.c \
+ ../../dfu-cc.pb.h ../../../../../external/nano-pb/pb.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\machine\_default_types.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\features.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\_newlib_version.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\_intsup.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\_stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\lib\gcc\arm-none-eabi\6.3.1\include\stdbool.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\string.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\newlib.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\config.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\machine\ieeefp.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\reent.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\_types.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\machine\_types.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\lock.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\cdefs.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\string.h
+
+../../dfu-cc.pb.h:
+
+../../../../../external/nano-pb/pb.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\stdint.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\machine\_default_types.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\features.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\_newlib_version.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\_intsup.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\_stdint.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\lib\gcc\arm-none-eabi\6.3.1\include\stdbool.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\string.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\_ansi.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\newlib.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\config.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\machine\ieeefp.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\reent.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\_ansi.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\_types.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\machine\_types.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\lock.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\cdefs.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q1-update\arm-none-eabi\include\sys\string.h:
---
+++ bootloader_secure/pca10040_debug/arm5_no_packs/_build/dfu_public_key.d
@@ -0,0 +1,3 @@
+.\_build\dfu_public_key.o: ..\..\dfu_public_key.c
+.\_build\dfu_public_key.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\dfu_public_key.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.11.1\Device\Include\compiler_abstraction.h
